81c3c99dbc66f8e8ce54bd7dccf64ba73c1e7c3c
SanektAnnadate <sanketannadatejain@gmail.com>
2023-05-04 15:42:24 +0530

    Movies section completed

--- src/app/components/MovieCard.js
+++ src/app/components/MovieCard.js
@@ -1,10 +1,24 @@
-import React from 'react';
-
-const MovieCard = () => {
+import styles from "@/app/styles/common.module.css"
+import Link from "next/link"
+import Image from "next/image"
+const MovieCard = (curElem) => {
+    const {id,type,title,synopsis} = curElem.jawSummary;
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className={styles.card}>
+                <div className={styles.card_image}>
+                    <Image src={curElem.jawSummary.backgroundImage.url} width={260} height={200}/>
+                </div>
+
+                <div className={styles.card_data}>
+                    <h2>{title}</h2>
+                    <p>{synopsis}</p>
+                    <Link href={`/movies/${id}`}>
+                        <button>Read more</button>
+                    </Link>
+                </div>
+            </div>
+        </>
     );
 };
 
--- src/app/movies/page.js
+++ src/app/movies/page.js
@@ -1,43 +1,42 @@
-import Link from "next/link";
-import axios from "axios";
+import React from 'react';
+import MovieCard from "@/app/components/MovieCard";
+import styles from "@/app/styles/common.module.css"
 
-const movies = async () => {
-  const axios = require("axios");
+const Movies = async () => {
 
-  const options = {
-    method: "GET",
-    url: "https://netflix54.p.rapidapi.com/search/",
-    params: {
-      query: "stranger",
-      offset: "0",
-      limit_titles: "50",
-      limit_suggestions: "20",
-      lang: "en",
-    },
-    headers: {
-      "X-RapidAPI-Key": "000a846d75msh94e4fbb1650702cp1f6fb7jsnf7b5429b07bb",
-      "X-RapidAPI-Host": "netflix54.p.rapidapi.com",
-    },
-  };
+    await new Promise(resolve => setTimeout(resolve, 2000));
 
-  try {
-    const response = await axios.request(options);
-    // console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
 
-  return (
-    <>
-      <section className={`${styles.movieSection}`}>
-        <div className={`${styles.container}`}>
-          <h1>Series of Movies</h1>
-          {response.map((curElem) => {
-            return <MovieCard key={curElem.id} {...curElem} />;
-          })}
-        </div>
-      </section>
-    </>
-  );
+    const url = process.env.RAPID_KEY;
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '000a846d75msh94e4fbb1650702cp1f6fb7jsnf7b5429b07bb',
+            'X-RapidAPI-Host': 'netflix54.p.rapidapi.com'
+          }
+    };
+
+    const res = await fetch(url, options);
+    const data = await res.json();
+    const main_data = data.titles;
+
+    return (
+        <>
+            <section className={styles.movieSection}>
+                <div className={styles.container}>
+                    <h1>Series & Movie</h1>
+                    <div className={styles.card_section}>
+                        {
+                            main_data.map((curElem) => {
+                                return <MovieCard key={curElem.id} {...curElem} />
+                            })
+                        }
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 };
-export default movies;
+
+export default Movies;
